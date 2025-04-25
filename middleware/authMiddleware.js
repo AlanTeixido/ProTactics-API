31824512aa39aea80fabb93b03e2c8997c74afb6
AlanTeixido <alan.teixido.7e7@itb.cat>
2025-04-25 16:38:35 +0200

    refactor: Update authMiddleware to use async/await for database query

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+const db = require("../requests/db"); // Asegúrate que esto apunta a tu conexión
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     const token = req.header("Authorization");
 
     if (!token) {
@@ -9,11 +10,22 @@ module.exports = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+        const result = await db.query(
+            "SELECT entrenador_id, correo, tipo, club_id FROM entrenadores WHERE entrenador_id = $1",
+            [decoded.id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(401).json({ error: "Usuari no trobat." });
+        }
+
         req.user = {
-            id: decoded.id,
-            tipo: decoded.tipo, 
-            correo: decoded.correo,
-        };        
+            id: result.rows[0].entrenador_id,
+            correo: result.rows[0].correo,
+            tipo: result.rows[0].tipo,
+            club_id: result.rows[0].club_id,
+        };
+
         next();
     } catch (error) {
         console.error("❌ Error verificando token:", error);
