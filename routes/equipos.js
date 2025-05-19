81ef31c106d80a53d344c91f6a379d8494858a52
Alan Teixido <Alan.Teixido@quantion.com>
2025-05-20 00:29:21 +0200

    feat: Add endpoint to retrieve teams based on user type

--- routes/equipos.js
+++ routes/equipos.js
@@ -16,4 +16,12 @@ router.get('/entrenador', authMiddleware, obtenerEquiposPorEntrenador);
 router.put('/:id', authMiddleware, editarEquipo);
 router.delete('/:id', authMiddleware, eliminarEquipo);
 
+router.get('/mis-equipos', authMiddleware, (req, res) => {
+  const { tipo } = req.user;
+  if (tipo === 'club') return obtenerEquipos(req, res);
+  if (tipo === 'entrenador') return obtenerEquiposPorEntrenador(req, res);
+  return res.status(403).json({ error: 'Tipus d\'usuari no suportat.' });
+});
+
+
 module.exports = router;
